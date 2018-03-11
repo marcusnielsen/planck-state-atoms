c77e5ad1397f9fea8c274caaede17895eed6ca1e
Marcus Nielsen <me@marcusnielsen.org>
2018-03-11 12:31:56 +0100

    add replay

--- src/chaos-monkey.js
+++ src/chaos-monkey.js
@@ -1,42 +1,44 @@
 import Rx from "rxjs";
 
-export const makeReplayStream = props => {
-  const { actionLog, buttons } = props;
+const replayAllRec = (buttons, replays, prevTimestamp, index) => {
+  const replay = replays[index];
+  const { timestamp, componentIndex, actionName } = replay;
+  const action = buttons[componentIndex].actions[actionName];
 
-  const result = actionLog
+  // NOTE: Only replay actions. Some actionStreams are reactions and does/should not have an action.
+  // All reactions will happen any way.
+  setTimeout(() => {
+    if (action) {
+      action();
+    }
+    if (index < replays.length - 1) {
+      replayAllRec(buttons, replays, timestamp, index + 1);
+    }
+  }, timestamp - prevTimestamp);
+};
+
+export const makeReplayStream = props => {
+  const { actionsLog, buttons } = props;
+  const replays = actionsLog
     .split("\n")
     .filter(row => row && !row.includes(".js:"))
     .map(row => {
       const [indexAndAction, timestamp] = row.split("T");
-      const [index, action] = indexAndAction.split("#");
+      const [componentIndex, actionName] = indexAndAction.split("#");
       return {
-        delay: timestamp,
-        index,
-        action
+        timestamp,
+        componentIndex,
+        actionName
       };
-    })
-    .map(a => console.log(a) || a)
-    .forEach(props => {
-      const action = buttons[props.index].actions[props.action];
-      if (!action) {
-        // It's an actionStream without an action. It will happen by itself.
-        return;
-      }
-      action();
     });
+
+  replayAllRec(buttons, replays, replays[0].timestamp, 0);
 };
 
 window.makeReplayStream = makeReplayStream;
 
 export const makeChaosMonkey = props => {
   const { buttons } = props;
-
-  const actions = [].concat(...buttons.map(b => Object.values(b.actions)));
-
-  Rx.Observable.interval(2000).forEach(() => {
-    const actionIndex = Math.floor(Math.random() * actions.length);
-    actions[actionIndex]();
-  });
 
   const actionStreams = [].concat(
     ...buttons.map((b, i) =>
@@ -46,5 +48,26 @@ export const makeChaosMonkey = props => {
     )
   );
 
-  Rx.Observable.merge(...actionStreams).forEach(data => console.log(data));
+  const allActionsStream = Rx.Observable.merge(...actionStreams);
+
+  allActionsStream.forEach(data => console.log(data));
+
+  const actionsLog = localStorage.getItem("actionsLog");
+
+  if (actionsLog) {
+    makeReplayStream({ actionsLog: actionsLog.replace(/;/g, "\n"), buttons });
+  } else {
+    allActionsStream.forEach(data => {
+      localStorage.setItem(
+        "actionsLog",
+        (localStorage.getItem("actionsLog") || "") + ";" + data
+      );
+    });
+    // const actions = [].concat(...buttons.map(b => Object.values(b.actions)));
+
+    // Rx.Observable.interval(2000).forEach(() => {
+    //   const actionIndex = Math.floor(Math.random() * actions.length);
+    //   actions[actionIndex]();
+    // });
+  }
 };
